dda31ac3a64e5b236dd405b02f80dda1f3858f9f
Angus <leung.mka@gmail.com>
2021-09-23 15:29:41 -0400

    Cleaned up project and task classes

--- src/index.js
+++ src/index.js
@@ -19,23 +19,7 @@ class Task {
         this.name = name;
         this.complete = false;
     }
-
-    setTask(name) {
-        this.name = name;
-    }
-
-    getTask() {
-        return this.name;
-    }
 }
-
-let a = new Task('test2');
-let newProject = new Project('testproject');
-newProject.addTask(a);
-let b = new Task('test3 i guess');
-newProject.addTask(b);
-console.log(newProject.tasks[0].complete);
-
 
 (function createListContainer() {
     let container = document.createElement('div');
@@ -46,10 +30,9 @@ console.log(newProject.tasks[0].complete);
 })();
 
 
-
 function domRender(task) {
     let container = document.querySelector('#List-Container');
-    let name = task.getTask();
+    let name = task.name;
     let status = task.complete;
     let taskDate = '10/15/2021';
 
@@ -60,6 +43,10 @@ function domRender(task) {
     input.classList = 'form-check-input flex-shrink-0';
     input.type = 'checkbox';
     input.checked = status;
+    input.addEventListener('click', (e) => {
+        task.complete = !task.complete;
+        console.log(task.complete)
+    });
     input.style = 'font-size: 1.375em;';
     
     const span = document.createElement('span');
@@ -85,6 +72,15 @@ function domRender(task) {
 
 };
 
+
+let a = new Task('test2');
+let newProject = new Project('testproject');
+newProject.addTask(a);
+let b = new Task('test3 i guess');
+newProject.addTask(b);
+console.log(newProject.tasks);
+
+newProject.tasks.forEach((task) => domRender(task));
 domRender(a);
 domRender(b);
 
